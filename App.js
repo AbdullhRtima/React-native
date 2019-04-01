cd0d7360dcd3912ffaf4a915db99ed9ebaf386c0
AbdullhRtima <abood.art.059@gmail.com>
2019-04-01 09:12:41 +0300

    V 1.0.0

---
+++ App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Explore from './screens/Explore'
+import Saved from './screens/Saved'
+import Inbox from './screens/Inbox'
+
+
+export default createBottomTabNavigator({
+  Explore: {
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'EXPLORE',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-search-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Saved: {
+    screen: Saved,
+    navigationOptions: {
+      tabBarLabel: 'SAVED',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-heart-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Inbox: {
+    screen: Inbox,
+    navigationOptions: {
+      tabBarLabel: 'INBOX',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-chatboxes-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile: {
+    screen: Inbox,
+    navigationOptions: {
+      tabBarLabel: 'PROFILE',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-person-outline" color={tintColor} size={24} />
+      )
+    }
+  }
+}, {
+    tabBarOptions: {
+      activeTintColor: 'red',
+      inactiveTintColor: 'grey',
+      style: {
+        backgroundColor: 'white',
+        borderTopWidth: 0,
+        shadowOffset: { width: 5, height: 3 },
+        shadowColor: 'black',
+        shadowOpacity: 0.5,
+        elevation: 5
+      }
+    }
+  })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
